cef9e8e6ddbb12ff77903d0bf927264669ff56a0
JMan <justinvang07@gmail.com>
2021-02-09 21:17:07 -0600

    created another burger.js, css, and updated index/burger-block handlebars

---
+++ public/assets/js/burger.js
@@ -0,0 +1,45 @@
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+$(function() {
+  $(".eatMe").on("click", function(event) {
+    var id = $(this).data("id");
+    var newStatus = $(this).data("newstatus");
+
+    var newStatusState = {
+      devoured: newStatus
+    };
+
+    // Send the PUT request.
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+      data: newStatusState
+    }).then(
+      function() {
+        console.log("changed burger to", newStatus);
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
+
+  $(".create-form").on("submit", function(event) {
+    // Make sure to preventDefault on a submit event.
+    event.preventDefault();
+
+    var newBurger = {
+      name: $("#ca").val().trim(),
+      devoured: $("[name=devoured]:checked").val().trim()
+    };
+
+    // Send the POST request.
+    $.ajax("/api/burgers", {
+      type: "POST",
+      data: newBurger
+    }).then(
+      function() {
+        console.log("created new Burger");
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
+});
