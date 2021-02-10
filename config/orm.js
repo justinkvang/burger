8480ea5589875d896d95cce79a6709bb263b119c
JMan <justinvang07@gmail.com>
2021-02-09 20:20:42 -0600

    adding logic and code

--- config/orm.js
+++ config/orm.js
@@ -1,12 +1,20 @@
 var connection = require("./connection.js");
 
 var orm = {
-    selectAll: function() {
-
+    selectAll: function(input, col) {
+        var queryString = "SELECT ?? FROM ??";
+        connection.query(queryString[input, col], function(err, res) {
+            if (err) throw err;
+            console.log(res);
+        });
     },
 
-    insertOne: function() {
-
+    insertOne: function(id, name, devoured) {
+        var queryString = "INSERT INTO ?? VALUES ??";
+        connection.query(queryString[id, name, devoured], function(err, res) {
+            if (err) throw err;
+            console.log(res);
+        });
     },
 
     updateOne: function() {
--- models/burger.js
+++ models/burger.js
@@ -3,4 +3,12 @@ var orm = require("../config/orm.js");
 // write ORM code
 // see act 13.16
 
-module.exports = burgers;
+var burger = {
+    selectAll: function(cb) {
+      orm.selectAll("burgers", function(res) { // this is calling the tables 'burgers' from database
+        cb(res);
+      });
+    },
+}
+
+module.exports = burger;
