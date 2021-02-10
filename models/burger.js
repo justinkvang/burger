6d3d09176c1ba8c74830a8b2d3f3b53a868e3359
JMan <justinvang07@gmail.com>
2021-02-09 20:48:00 -0600

    updated burgers.js with functions

--- models/burger.js
+++ models/burger.js
@@ -1,14 +1,24 @@
+// Import the ORM to create functions that will interact with the database.
 var orm = require("../config/orm.js");
 
-// write ORM code
-// see act 13.16
+var cat = {
+  selectAll: function(cb) {
+    orm.selectAll("burgers", function(res) {
+      cb(res);
+    });
+  },
+  // The variables cols and vals are arrays.
+  insertOne: function(cols, vals, cb) {
+    orm.insertOne("burgers", cols, vals, function(res) {
+      cb(res);
+    });
+  },
+  updateOne: function(objColVals, condition, cb) {
+    orm.updateOne("burgers", objColVals, condition, function(res) {
+      cb(res);
+    });
+  }
+};
 
-var burger = {
-    selectAll: function(cb) {
-      orm.selectAll("burgers", function(res) { // this is calling the tables 'burgers' from database
-        cb(res);
-      });
-    }
-}
-
-module.exports = burger;
+// Export the database functions for the controller (catsController.js).
+module.exports = cat;
