8b12d472fc5428f75d1f149c2354012e401df4df
JMan <justinvang07@gmail.com>
2021-02-10 19:02:48 -0600

    updated stuff

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -17,7 +17,7 @@ router.get("/", function(req, res) {
 });
 
 router.post("/api/burgers", function(req, res) {
-  cat.insertOne([
+  burger.insertOne([
     "burger_name", "devoured"
   ], [
     req.body.burger_name, req.body.devoured
